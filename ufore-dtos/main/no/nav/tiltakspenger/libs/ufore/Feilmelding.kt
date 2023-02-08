ace43be802c2c2085f63f73904fd2e55a68fa753
Richard Martinsen <github@martinsen.org>
2023-02-08 13:55:12 +0100

    legg til feilmelding i uføre

--- ufore-dtos/main/no/nav/tiltakspenger/libs/ufore/Feilmelding.kt
+++ ufore-dtos/main/no/nav/tiltakspenger/libs/ufore/Feilmelding.kt
@@ -1,5 +1,6 @@
 package no.nav.tiltakspenger.libs.ufore
 
 enum class Feilmelding(val message: String) {
-    UkjentFeil("Ukjent feil i ufore")
+    UkjentFeil("Ukjent feil i ufore"),
+    UgyldigIdent("Ugyldig ident"),
 }

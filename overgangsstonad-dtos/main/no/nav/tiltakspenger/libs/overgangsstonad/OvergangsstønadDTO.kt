1f373d09274853d0bea739a6cfcd44470d1e867b
Richard Martinsen <github@martinsen.org>
2023-02-15 14:47:42 +0100

    Fjern ubrukte props i OvergangsstønadDTO

--- overgangsstonad-dtos/main/no/nav/tiltakspenger/libs/overgangsstonad/OvergangsstønadDTO.kt
+++ overgangsstonad-dtos/main/no/nav/tiltakspenger/libs/overgangsstonad/OvergangsstønadDTO.kt
@@ -1,9 +1,5 @@
 package no.nav.tiltakspenger.libs.overgangsstonad
 
-import java.time.LocalDate
-
 data class OvergangsstønadDTO(
     val perioder: List<OvergangsstønadPeriodeDTO>,
-    val datoUfor: LocalDate?,
-    val virkDato: LocalDate?,
 )

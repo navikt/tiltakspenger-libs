89b3a8545741c3f06edd5ef6f9826fc719fc7745
Sneha Desai <snehadesai@Sneha-sin-MacBook-Pro.local>
2024-04-09 04:59:23 +0200

    F Legg flere feilmelding til enum Filemellding under person

--- person-dtos/main/no/nav/tiltakspenger/libs/person/Feilmelding.kt
+++ person-dtos/main/no/nav/tiltakspenger/libs/person/Feilmelding.kt
@@ -2,4 +2,14 @@ package no.nav.tiltakspenger.libs.person
 
 enum class Feilmelding(val message: String) {
     PersonIkkeFunnet("Fant ikke person i PDL"),
+    NavnIkkeFunnet("Fant ikke navn i PDL"),
+    NavnKunneIkkeAvklares("Navn kunne ikke avklares fra PDL respons"),
+    FødselKunneIkkeAvklares("Fødsel kunne ikke avklares fra PDL respons"),
+    AdressebeskyttelseKunneIkkeAvklares("Adressebeskyttelse kunne ikke avklares fra PDL respons"),
+    NetworkError("PDL er nede!!"),
+    UkjentFeil("Uhåndtert feil oppsto ved å hente person fra PDL"),
+    ResponsManglerPerson("Person finnes ikke i PDL"),
+    SerializationException("Feil ved serializering av PDL data"),
+    GraderingKunneIkkeAvklares("Kunne ikke avklare gradering"),
+    AzureAuthFailureException("Kunne ikke autentisere mot Azure"),
 }

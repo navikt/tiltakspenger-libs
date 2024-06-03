300201993215c36589f5ac42ce86c8f17cc3cb07
Erlend Vige <erlend.edvardsen.vige@nav.no>
2024-06-03 13:48:43 +0200

    R Bruke trekkFra i Periode.kompletter 😌
    Co-authored-by: KarlEvaldNav <Karl.Evald.Wigestrand@nav.no>
    Co-authored-by: richardmartinsen <Richard.Martinsen@nav.no>

--- periodisering/main/no/nav/tiltakspenger/libs/periodisering/Periode.kt
+++ periodisering/main/no/nav/tiltakspenger/libs/periodisering/Periode.kt
@@ -55,26 +55,8 @@ class Periode(fra: LocalDate, til: LocalDate) {
             throw IllegalArgumentException("Alle periodene må være innenfor hoved-perioden")
         }
 
-        var mangelPeriode = Periode(fra = this.fra, til = this.til)
-        val nyePerioder = mutableListOf<Periode>()
-        overlappendePerioder.forEach { periode ->
-            if (mangelPeriode.fra.isBefore(periode.fra)) {
-                nyePerioder.add(Periode(fra = mangelPeriode.fra, til = periode.fra.minusDays(1)))
-                nyePerioder.add(periode)
-            } else if (mangelPeriode.fra.isEqual(periode.fra)) {
-                nyePerioder.add(periode)
-            }
-            if (periode.til.isBefore(this.til)) {
-                mangelPeriode = Periode(fra = periode.til.plusDays(1), til = this.til)
-            }
-        }
-
-        val nyePerioderHarHullPåSlutten = nyePerioder.maxOf { it.til }.isBefore(this.til)
-        if (nyePerioderHarHullPåSlutten) {
-            nyePerioder.add(mangelPeriode)
-        }
-
-        return nyePerioder
+        val mangelPerioder = this.trekkFra(perioder)
+        return (mangelPerioder + perioder).sortedBy { it.fra }
     }
 
     fun mergeInnIPerioder(perioder: List<Periode>): List<Periode> {

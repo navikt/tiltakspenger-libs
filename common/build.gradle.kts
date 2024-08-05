3d6ec0bcb6725bd2bd7e8b2c4c83fd67f62779dd
John Andre Hestad <john.andre.hestad@nav.no>
2024-08-05 14:14:38 +0200

    Flytt Ulid fra vedtak->libs

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -2,6 +2,7 @@ dependencies {
     implementation("io.github.microutils:kotlin-logging-jvm:3.0.5")
     implementation("io.arrow-kt:arrow-core:1.2.4")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
+    implementation("com.aallam.ulid:ulid-kotlin:1.3.0")
 }
 
 configurations.all {

433a6d2aef4cbd614a60d4997f230642458bd526
John Andre Hestad <john.andre.hestad@nav.no>
2024-08-16 15:00:43 +0200

    Legg til TestSessionFactory i libs

--- test-common/build.gradle.kts
+++ test-common/build.gradle.kts
@@ -1,5 +1,13 @@
+val kotestVersion = "5.9.1"
+
 dependencies {
     implementation(project(":common"))
+    implementation(project(":persistering:persistering-domene"))
+
+    implementation("io.kotest:kotest-assertions-core:$kotestVersion")
+    implementation("io.kotest:kotest-assertions-json:$kotestVersion")
+    implementation("io.kotest.extensions:kotest-assertions-arrow:1.4.0")
+    implementation("io.kotest:kotest-extensions:$kotestVersion")
 }
 
 configurations.all {

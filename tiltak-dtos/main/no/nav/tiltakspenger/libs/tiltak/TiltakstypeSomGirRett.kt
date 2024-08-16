db1f03852b02a4b7cfc93799e6172f9106576ca2
karlevaldnav <karl.evald.wigestrand@nav.no>
2024-08-16 13:16:14 +0200

    R Fjern ubrukt mapper: fra String til TiltakstypeSomGirRett. Bruker heller et eget database enum i de repoened det trengs

--- tiltak-dtos/main/no/nav/tiltakspenger/libs/tiltak/TiltakstypeSomGirRett.kt
+++ tiltak-dtos/main/no/nav/tiltakspenger/libs/tiltak/TiltakstypeSomGirRett.kt
@@ -1,7 +1,5 @@
 package no.nav.tiltakspenger.libs.tiltak
 
-import java.lang.RuntimeException
-
 enum class TiltakstypeSomGirRett {
     /** ARBEIDSFORBEREDENDE_TRENING i Komet per 2024-08-14  */
     ARBEIDSFORBEREDENDE_TRENING,
@@ -36,30 +34,4 @@ enum class TiltakstypeSomGirRett {
     OPPFØLGING,
     UTVIDET_OPPFØLGING_I_NAV,
     UTVIDET_OPPFØLGING_I_OPPLÆRING,
-    ;
-
-    companion object {
-        fun fraString(value: String): TiltakstypeSomGirRett {
-            return when (value) {
-                "ARBEIDSFORBEREDENDE_TRENING" -> TiltakstypeSomGirRett.ARBEIDSFORBEREDENDE_TRENING
-                "ARBEIDSRETTET_REHABILITERING" -> TiltakstypeSomGirRett.ARBEIDSRETTET_REHABILITERING
-                "ARBEIDSTRENING" -> TiltakstypeSomGirRett.ARBEIDSTRENING
-                "AVKLARING" -> TiltakstypeSomGirRett.AVKLARING
-                "DIGITAL_JOBBKLUBB" -> TiltakstypeSomGirRett.DIGITAL_JOBBKLUBB
-                "ENKELTPLASS_AMO" -> TiltakstypeSomGirRett.ENKELTPLASS_AMO
-                "ENKELTPLASS_VGS_OG_HØYERE_YRKESFAG" -> TiltakstypeSomGirRett.ENKELTPLASS_VGS_OG_HØYERE_YRKESFAG
-                "FORSØK_OPPLÆRING_LENGRE_VARIGHET" -> TiltakstypeSomGirRett.FORSØK_OPPLÆRING_LENGRE_VARIGHET
-                "GRUPPE_AMO" -> TiltakstypeSomGirRett.GRUPPE_AMO
-                "GRUPPE_VGS_OG_HØYERE_YRKESFAG" -> TiltakstypeSomGirRett.GRUPPE_VGS_OG_HØYERE_YRKESFAG
-                "HØYERE_UTDANNING" -> TiltakstypeSomGirRett.HØYERE_UTDANNING
-                "INDIVIDUELL_JOBBSTØTTE" -> TiltakstypeSomGirRett.INDIVIDUELL_JOBBSTØTTE
-                "INDIVIDUELL_KARRIERESTØTTE_UNG" -> TiltakstypeSomGirRett.INDIVIDUELL_KARRIERESTØTTE_UNG
-                "JOBBKLUBB" -> TiltakstypeSomGirRett.JOBBKLUBB
-                "OPPFØLGING" -> TiltakstypeSomGirRett.OPPFØLGING
-                "UTVIDET_OPPFØLGING_I_NAV" -> TiltakstypeSomGirRett.UTVIDET_OPPFØLGING_I_NAV
-                "UTVIDET_OPPFØLGING_I_OPPLÆRING" -> TiltakstypeSomGirRett.UTVIDET_OPPFØLGING_I_OPPLÆRING
-                else -> throw RuntimeException("Verdien '$value' matchet ikke med noen av tiltakstypene som gir rett til tiltakspenger")
-            }
-        }
-    }
 }

c936638bb8a8667a1fda9cd6c0d475eef0877c18
Richard Martinsen <github@martinsen.org>
2024-09-05 09:58:50 +0200

    R - Endre prop foedsel til foedselsdato mot pdl

--- personklient/personklient-infrastruktur/main/no/nav/tiltakspenger/libs/personklient/pdl/dto/PdlPerson.kt
+++ personklient/personklient-infrastruktur/main/no/nav/tiltakspenger/libs/personklient/pdl/dto/PdlPerson.kt
@@ -2,7 +2,7 @@ package no.nav.tiltakspenger.libs.personklient.pdl.dto
 
 data class PdlPerson(
     val navn: List<Navn>,
-    val foedsel: List<Fødsel>,
+    val foedselsdato: List<Fødsel>,
     val adressebeskyttelse: List<Adressebeskyttelse>,
     val forelderBarnRelasjon: List<ForelderBarnRelasjon>,
 )

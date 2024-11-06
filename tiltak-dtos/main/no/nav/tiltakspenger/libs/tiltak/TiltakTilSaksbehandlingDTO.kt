dda4b410de9bf1289ea0fc1cd1dcd352f45d618b
Benedicte Myrvoll <benemyr17@gmail.com>
2024-11-06 09:58:08 +0100

    Gjør endringer i DTO for tiltak som hentes for saksbehandling

--- tiltak-dtos/main/no/nav/tiltakspenger/libs/tiltak/TiltakTilSaksbehandlingDTO.kt
+++ tiltak-dtos/main/no/nav/tiltakspenger/libs/tiltak/TiltakTilSaksbehandlingDTO.kt
@@ -1,19 +1,16 @@
 package no.nav.tiltakspenger.libs.tiltak
-
 import no.nav.tiltakspenger.libs.tiltak.TiltakResponsDTO.DeltakerStatusDTO
+import no.nav.tiltakspenger.libs.tiltak.TiltakResponsDTO.TiltakType
 import java.time.LocalDate
-import java.time.LocalDateTime
 
 data class TiltakTilSaksbehandlingDTO(
     val id: String,
-    val deltakelseFom: LocalDate,
-    val deltakelseTom: LocalDate,
-    val gjennomføringId: String,
+    val deltakelseFom: LocalDate?,
+    val deltakelseTom: LocalDate?,
     val typeNavn: String,
-    val typeKode: String,
+    val typeKode: TiltakType,
     val deltakelseStatus: DeltakerStatusDTO,
     val deltakelsePerUke: Float?,
     val deltakelseProsent: Float?,
     val kilde: String,
-    val registrertDato: LocalDateTime,
 )

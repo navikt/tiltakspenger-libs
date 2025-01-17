36ed9e1f20c8dfaa7a1effe0155f6e8e61b93f16
Anders B Nomerstad <anders@nomerstad.com>
2025-01-17 15:29:24 +0100

    Legger til List.nonDistinctBy

---
+++ common/test/no.nav.tiltakspenger.libs.common.felles/ListExTest.kt
@@ -0,0 +1,25 @@
+package no.nav.tiltakspenger.libs.common.felles
+
+import io.kotest.matchers.collections.shouldHaveSize
+import no.nav.tiltakspenger.libs.common.nonDistinctBy
+import org.junit.jupiter.api.Test
+
+internal class ListExTest {
+    private data class Dummy(val num: Int, val text: String)
+
+    private val liste = listOf(
+        Dummy(1, "foo"),
+        Dummy(2, "foo"),
+        Dummy(3, "bar"),
+    )
+
+    @Test
+    fun `should have no non-distinct num values`() {
+        liste.nonDistinctBy { it.num }.shouldHaveSize(0)
+    }
+
+    @Test
+    fun `should have two non-distinct text values`() {
+        liste.nonDistinctBy { it.text }.shouldHaveSize(2)
+    }
+}

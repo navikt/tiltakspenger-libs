ee7040351ad5cb37679cac10ff134042e0d2c279
John Andre Hestad <john.andre.hestad@nav.no>
2025-02-24 18:46:23 +0100

    Rename HendelseId->MeldeperiodeId

---
+++ common/main/no/nav/tiltakspenger/libs/common/MeldeperiodeId.kt
@@ -0,0 +1,21 @@
+package no.nav.tiltakspenger.libs.common
+
+import ulid.ULID
+import java.util.UUID
+
+data class MeldeperiodeId private constructor(
+    private val ulid: UlidBase,
+) : Ulid by ulid {
+    companion object {
+        private const val PREFIX = "meldeperiode"
+
+        fun random() = MeldeperiodeId(ulid = UlidBase("${PREFIX}_${ULID.randomULID()}"))
+
+        fun fromString(stringValue: String): MeldeperiodeId {
+            require(stringValue.startsWith(PREFIX)) { "Prefix må starte med $PREFIX. Dette er nok ikke en MeldeperiodeId ($stringValue)" }
+            return MeldeperiodeId(ulid = UlidBase(stringValue))
+        }
+
+        fun fromUUID(uuid: UUID) = MeldeperiodeId(ulid = UlidBase("${PREFIX}_${uuidToUlid(uuid)}"))
+    }
+}

b916877be5de5b6f33a44b1a0f9ddbbff01d41b3
ramziabuqassim <ramzi.abu.qassim@nav.no>
2025-03-24 16:01:18 +0100

    nå() tar inn clock

--- common/main/no/nav/tiltakspenger/libs/common/LocalDateTimeEx.kt
+++ common/main/no/nav/tiltakspenger/libs/common/LocalDateTimeEx.kt
@@ -1,9 +1,10 @@
 package no.nav.tiltakspenger.libs.common
 
+import java.time.Clock
 import java.time.LocalDateTime
 import java.time.temporal.ChronoUnit
 
 /**
  * Truncater til mikrosekunder for å være kompatibel med PostgreSQL.
  */
-fun nå(): LocalDateTime = LocalDateTime.now().truncatedTo(ChronoUnit.MICROS)
+fun nå(clock: Clock): LocalDateTime = LocalDateTime.now(clock).truncatedTo(ChronoUnit.MICROS)

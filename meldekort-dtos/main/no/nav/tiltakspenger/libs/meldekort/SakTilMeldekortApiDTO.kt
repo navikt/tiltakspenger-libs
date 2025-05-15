bae9b054b9d9e10c956b2d2f9da913521a8eaa7d
Anders B Nomerstad <anders@nomerstad.com>
2025-05-15 11:05:28 +0200

    DTO for sak til meldekort-api

---
+++ meldekort-dtos/main/no/nav/tiltakspenger/libs/meldekort/SakTilMeldekortApiDTO.kt
@@ -0,0 +1,26 @@
+package no.nav.tiltakspenger.libs.meldekort
+
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+/**
+ *  Sak med meldeperioder som sendes fra saksbehandling-api til meldekort-api når det fattes et nytt vedtak på saken
+ *  @param meldeperioder Gjeldende versjon av alle meldeperioder på saken
+ * */
+data class SakTilMeldekortApiDTO(
+    val fnr: String,
+    val sakId: String,
+    val saksnummer: String,
+    val meldeperioder: List<Meldeperiode>,
+) {
+    data class Meldeperiode(
+        val id: String,
+        val kjedeId: String,
+        val versjon: Int,
+        val opprettet: LocalDateTime,
+        val fraOgMed: LocalDate,
+        val tilOgMed: LocalDate,
+        val antallDagerForPeriode: Int,
+        val girRett: Map<LocalDate, Boolean>,
+    )
+}

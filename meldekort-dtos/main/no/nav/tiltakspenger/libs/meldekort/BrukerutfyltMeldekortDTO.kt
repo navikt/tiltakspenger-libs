0e959d1dcbb52a06b095174bb5dcd60e5ee6a933
John Andre Hestad <john.andre.hestad@nav.no>
2025-06-16 20:44:04 +0200

    Endre meldekortstatuser til å være lik de i meldekort og meldekort-api

--- meldekort-dtos/main/no/nav/tiltakspenger/libs/meldekort/BrukerutfyltMeldekortDTO.kt
+++ meldekort-dtos/main/no/nav/tiltakspenger/libs/meldekort/BrukerutfyltMeldekortDTO.kt
@@ -21,11 +21,12 @@ data class BrukerutfyltMeldekortDTO(
     val journalpostId: String,
 ) {
     enum class Status {
-        DELTATT,
+        DELTATT_UTEN_LØNN_I_TILTAKET,
+        DELTATT_MED_LØNN_I_TILTAKET,
         FRAVÆR_SYK,
         FRAVÆR_SYKT_BARN,
+        FRAVÆR_GODKJENT_AV_NAV,
         FRAVÆR_ANNET,
-        IKKE_DELTATT,
-        IKKE_REGISTRERT,
+        IKKE_BESVART,
     }
 }
--- meldekort-dtos/test/no/nav/tiltakspenger/libs/meldekort/BrukerutfyltMeldekortDTOTest.kt
+++ meldekort-dtos/test/no/nav/tiltakspenger/libs/meldekort/BrukerutfyltMeldekortDTOTest.kt
@@ -4,12 +4,10 @@ import io.kotest.assertions.json.shouldEqualJson
 import io.kotest.matchers.shouldBe
 import no.nav.tiltakspenger.libs.json.deserialize
 import no.nav.tiltakspenger.libs.json.serialize
-import no.nav.tiltakspenger.libs.meldekort.BrukerutfyltMeldekortDTO.Status.DELTATT
 import no.nav.tiltakspenger.libs.meldekort.BrukerutfyltMeldekortDTO.Status.FRAVÆR_ANNET
 import no.nav.tiltakspenger.libs.meldekort.BrukerutfyltMeldekortDTO.Status.FRAVÆR_SYK
 import no.nav.tiltakspenger.libs.meldekort.BrukerutfyltMeldekortDTO.Status.FRAVÆR_SYKT_BARN
-import no.nav.tiltakspenger.libs.meldekort.BrukerutfyltMeldekortDTO.Status.IKKE_DELTATT
-import no.nav.tiltakspenger.libs.meldekort.BrukerutfyltMeldekortDTO.Status.IKKE_REGISTRERT
+import no.nav.tiltakspenger.libs.meldekort.BrukerutfyltMeldekortDTO.Status.IKKE_BESVART
 import no.nav.tiltakspenger.libs.periodisering.Periode
 import no.nav.tiltakspenger.libs.periodisering.PeriodeDTO
 import org.junit.jupiter.api.Test
@@ -27,20 +25,20 @@ class BrukerutfyltMeldekortDTOTest {
         val journalpostId = "12345"
 
         val dager: Map<LocalDate, BrukerutfyltMeldekortDTO.Status> = buildMap {
-            put(periode.fraOgMed, DELTATT)
-            put(periode.fraOgMed.plusDays(1), DELTATT)
+            put(periode.fraOgMed, BrukerutfyltMeldekortDTO.Status.DELTATT_UTEN_LØNN_I_TILTAKET)
+            put(periode.fraOgMed.plusDays(1), BrukerutfyltMeldekortDTO.Status.DELTATT_UTEN_LØNN_I_TILTAKET)
             put(periode.fraOgMed.plusDays(2), FRAVÆR_ANNET)
             put(periode.fraOgMed.plusDays(3), FRAVÆR_SYKT_BARN)
             put(periode.fraOgMed.plusDays(4), FRAVÆR_SYK)
-            put(periode.fraOgMed.plusDays(5), IKKE_REGISTRERT)
-            put(periode.fraOgMed.plusDays(6), IKKE_REGISTRERT)
-            put(periode.fraOgMed.plusDays(7), DELTATT)
-            put(periode.fraOgMed.plusDays(8), IKKE_DELTATT)
-            put(periode.fraOgMed.plusDays(9), DELTATT)
-            put(periode.fraOgMed.plusDays(10), IKKE_DELTATT)
-            put(periode.fraOgMed.plusDays(11), IKKE_DELTATT)
-            put(periode.fraOgMed.plusDays(12), IKKE_REGISTRERT)
-            put(periode.tilOgMed, IKKE_REGISTRERT)
+            put(periode.fraOgMed.plusDays(5), IKKE_BESVART)
+            put(periode.fraOgMed.plusDays(6), IKKE_BESVART)
+            put(periode.fraOgMed.plusDays(7), BrukerutfyltMeldekortDTO.Status.DELTATT_UTEN_LØNN_I_TILTAKET)
+            put(periode.fraOgMed.plusDays(8), IKKE_BESVART)
+            put(periode.fraOgMed.plusDays(9), BrukerutfyltMeldekortDTO.Status.DELTATT_UTEN_LØNN_I_TILTAKET)
+            put(periode.fraOgMed.plusDays(10), IKKE_BESVART)
+            put(periode.fraOgMed.plusDays(11), IKKE_BESVART)
+            put(periode.fraOgMed.plusDays(12), IKKE_BESVART)
+            put(periode.tilOgMed, IKKE_BESVART)
         }
 
         val meldekort = BrukerutfyltMeldekortDTO(
@@ -67,20 +65,20 @@ class BrukerutfyltMeldekortDTOTest {
                 },
                 "mottatt": "2024-01-23T12:00:00",
                 "dager": {
-                    "2024-01-01": "DELTATT",
-                    "2024-01-02": "DELTATT",
+                    "2024-01-01": "DELTATT_UTEN_LØNN_I_TILTAKET",
+                    "2024-01-02": "DELTATT_UTEN_LØNN_I_TILTAKET",
                     "2024-01-03": "FRAVÆR_ANNET",
                     "2024-01-04": "FRAVÆR_SYKT_BARN",
                     "2024-01-05": "FRAVÆR_SYK",
-                    "2024-01-06": "IKKE_REGISTRERT",
-                    "2024-01-07": "IKKE_REGISTRERT",
-                    "2024-01-08": "DELTATT",
-                    "2024-01-09": "IKKE_DELTATT",
-                    "2024-01-10": "DELTATT",
-                    "2024-01-11": "IKKE_DELTATT",
-                    "2024-01-12": "IKKE_DELTATT",
-                    "2024-01-13": "IKKE_REGISTRERT",
-                    "2024-01-14": "IKKE_REGISTRERT"
+                    "2024-01-06": "IKKE_BESVART",
+                    "2024-01-07": "IKKE_BESVART",
+                    "2024-01-08": "DELTATT_UTEN_LØNN_I_TILTAKET",
+                    "2024-01-09": "IKKE_BESVART",
+                    "2024-01-10": "DELTATT_UTEN_LØNN_I_TILTAKET",
+                    "2024-01-11": "IKKE_BESVART",
+                    "2024-01-12": "IKKE_BESVART",
+                    "2024-01-13": "IKKE_BESVART",
+                    "2024-01-14": "IKKE_BESVART"
                   },
                 "journalpostId": "$journalpostId"
             }

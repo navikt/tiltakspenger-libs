21f65a6cb3c7431b147d3433079a8ecf813e356c
tiafiring <tia.firing@nav.no>
2025-09-30 10:39:13 +0200

    1412 flytter satser til libs så de kan brukes ifm datadeling også (#309)
    * 1412 flytter satser til libs så de kan brukes ifm datadeling også
    
    * 1412 legger til test og dokumentasjon

---
+++ satser/test/no/nav/tiltakspenger/libs/satser/SatserTest.kt
@@ -0,0 +1,108 @@
+package no.nav.tiltakspenger.libs.satser
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.matchers.shouldBe
+import no.nav.tiltakspenger.libs.dato.desember
+import no.nav.tiltakspenger.libs.dato.januar
+import no.nav.tiltakspenger.libs.periodisering.Periode
+import no.nav.tiltakspenger.libs.periodisering.inneholderOverlapp
+import org.junit.jupiter.api.Test
+import java.time.LocalDate
+
+internal class SatserTest {
+    @Test
+    fun `skal returnere korrekt sats for 2023`() {
+        val satsdag = Satser.sats(1.januar(2023))
+        satsdag.sats shouldBe 268
+        satsdag.satsRedusert shouldBe 201
+        satsdag.satsBarnetillegg shouldBe 52
+        satsdag.satsBarnetilleggRedusert shouldBe 39
+    }
+
+    @Test
+    fun `skal returnere korrekt sats for 2024`() {
+        val satsdag = Satser.sats(1.januar(2024))
+        satsdag.sats shouldBe 285
+        satsdag.satsRedusert shouldBe 214
+        satsdag.satsBarnetillegg shouldBe 53
+        satsdag.satsBarnetilleggRedusert shouldBe 40
+    }
+
+    @Test
+    fun `skal returnere korrekt sats for 2025`() {
+        val satsdag = Satser.sats(1.januar(2025))
+        satsdag.sats shouldBe 298
+        satsdag.satsRedusert shouldBe 224
+        satsdag.satsBarnetillegg shouldBe 55
+        satsdag.satsBarnetilleggRedusert shouldBe 41
+    }
+
+    @Test
+    fun `skal kaste exception for dato før 2023`() {
+        shouldThrow<IllegalArgumentException> {
+            Satser.sats(31.desember(2022))
+        }
+    }
+
+    @Test
+    fun `skal validere at satser er positive ved opprettelse`() {
+        shouldThrow<IllegalArgumentException> {
+            Sats(
+                periode = Periode(
+                    fraOgMed = LocalDate.now(),
+                    tilOgMed = LocalDate.now(),
+                ),
+                sats = -1,
+                satsRedusert = 100,
+                satsBarnetillegg = 100,
+                satsBarnetilleggRedusert = 100,
+            )
+        }
+
+        shouldThrow<IllegalArgumentException> {
+            Sats(
+                periode = Periode(
+                    fraOgMed = LocalDate.now(),
+                    tilOgMed = LocalDate.now(),
+                ),
+                sats = 100,
+                satsRedusert = -1,
+                satsBarnetillegg = 100,
+                satsBarnetilleggRedusert = 100,
+            )
+        }
+
+        shouldThrow<IllegalArgumentException> {
+            Sats(
+                periode = Periode(
+                    fraOgMed = LocalDate.now(),
+                    tilOgMed = LocalDate.now(),
+                ),
+                sats = 100,
+                satsRedusert = 100,
+                satsBarnetillegg = -1,
+                satsBarnetilleggRedusert = 100,
+            )
+        }
+
+        shouldThrow<IllegalArgumentException> {
+            Sats(
+                periode = Periode(
+                    fraOgMed = LocalDate.now(),
+                    tilOgMed = LocalDate.now(),
+                ),
+                sats = 100,
+                satsRedusert = 100,
+                satsBarnetillegg = 100,
+                satsBarnetilleggRedusert = -1,
+            )
+        }
+    }
+
+    @Test
+    fun `skal ikke kunne ha overlappende perioder for satser`() {
+        val satsperioder = Satser.satser.map { it.periode }
+
+        satsperioder.inneholderOverlapp() shouldBe false
+    }
+}

64ce7fb979cf8cefaaf50a5c503d016fb633e0a3
Henrik Solum <henrik.solum@nav.no>
2025-10-21 09:28:45 +0200

    1343 Send tiltaksarrangørens navn så det kan vises under saksbehandling
    Dette gjør DTOen nesten identisk med TiltakDTO, men velger å holde de adskilt da TiltakDTO brukes primært for henting av tiltak som systembruker, mens TiltakTilSaksbehandlingDTO brukes i tiltakspenger-saksbehandling-api

--- tiltak-dtos/main/no/nav/tiltakspenger/libs/tiltak/TiltakTilSaksbehandlingDTO.kt
+++ tiltak-dtos/main/no/nav/tiltakspenger/libs/tiltak/TiltakTilSaksbehandlingDTO.kt
@@ -1,14 +1,20 @@
 package no.nav.tiltakspenger.libs.tiltak
 import no.nav.tiltakspenger.libs.tiltak.TiltakResponsDTO.DeltakerStatusDTO
+import no.nav.tiltakspenger.libs.tiltak.TiltakResponsDTO.GjennomføringDTO
 import no.nav.tiltakspenger.libs.tiltak.TiltakResponsDTO.TiltakType
 import java.time.LocalDate
 
 data class TiltakTilSaksbehandlingDTO(
     val id: String,
+    // TODO Nullable frem til de gamle feltene ovenfor er faset ut i alle applikasjoner
+    val gjennomføring: GjennomføringDTO? = null,
+    @Deprecated("Bruk gjennomføring.id")
     val gjennomføringId: String?,
     val deltakelseFom: LocalDate?,
     val deltakelseTom: LocalDate?,
+    @Deprecated("Bruk gjennomføring.typeNavn")
     val typeNavn: String,
+    @Deprecated("Bruk gjennomføring.typeKode")
     val typeKode: TiltakType,
     val deltakelseStatus: DeltakerStatusDTO,
     val deltakelsePerUke: Float?,

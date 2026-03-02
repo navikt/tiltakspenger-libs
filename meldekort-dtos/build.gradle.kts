91ffd2f1e4e0facee4faa20fc3aeb6df9d0ef300
John Andre Hestad <john.andre.hestad@nav.no>
2026-03-02 17:15:20 +0100

    Fjern ubrukt tiltaksgruppe

--- meldekort-dtos/build.gradle.kts
+++ meldekort-dtos/build.gradle.kts
@@ -1,6 +1,5 @@
 dependencies {
     implementation(project(":json"))
-    implementation(project(":tiltak-dtos"))
     implementation(project(":periodisering"))
 
     testImplementation(project(":test-common"))

cad8f38e03e948ca75e31c09b6843777605c75ec
John Andre Hestad <john.andre.hestad@nav.no>
2026-03-27 14:59:07 +0100

    Fjern ubrukt/deprecated ErrorResponse.kt

--- ktor-common/main/no/nav/tiltakspenger/libs/ktor/common/ErrorResponse.kt
+++
@@ -1,9 +0,0 @@
-package no.nav.tiltakspenger.libs.ktor.common
-
-import io.ktor.http.HttpStatusCode
-
-@Deprecated("Bruk  no.nav.tiltakspenger.libs.ktor.common.ErrorJson")
-data class ErrorResponse(
-    val json: ErrorJsonBase,
-    val httpStatus: HttpStatusCode,
-)

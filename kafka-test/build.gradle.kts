e8e7ce701df53960e3e87182b5c4405cc385fa37
John Andre Hestad <john.andre.hestad@nav.no>
2026-07-30 17:54:12 +0200

    Legg til lz4-java avhengighet med passende constraints i build.gradle.kts

--- kafka-test/build.gradle.kts
+++ kafka-test/build.gradle.kts
@@ -5,6 +5,11 @@ plugins {
 dependencies {
     implementation(project(":logging"))
 
+    // Samme løft som i :kafka. Modulen deklarerer kafka-clients selv og arver derfor ikke constrainten derfra.
+    constraints {
+        implementation(libs.lz4.java)
+    }
+
     implementation(libs.kafka.clients)
     implementation(libs.testcontainers)
     implementation(libs.testcontainers.kafka)
